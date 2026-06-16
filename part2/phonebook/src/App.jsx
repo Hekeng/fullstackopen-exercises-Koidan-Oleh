6edddf6a6e2755b620896b605445c5c981483da1
Oleh Koidan <o.koidan@nextcontrol.de>
2026-06-16 11:04:24 +0200

    par2_2.14_save

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import axiosServices from './services/axiosServises'
 
 const Filter = ({ newSearch, handleSetSearch }) => {
   return (
@@ -28,24 +30,31 @@ const Persons = ({ personsToShow }) => {
   return (
     <>
       {personsToShow.map((person) => (
-        <p key={person.id}>
-          {person.name} {person.number}
-        </p>
+        <div key={person.id}>
+          <span>
+            {person.name} {person.number}
+          </span>
+          <button>delete</button>
+        </div>
       ))}
     </>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axiosServices.getAll().then((response) => {
+      console.log('promise fulfilled', response)
+      console.log('promise fulfilled')
+      setPersons(response)
+    })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -71,7 +80,11 @@ const App = () => {
     if (checkRepeatedName(newPerson.name)) {
       return
     } else {
-      setPersons(persons.concat(newPerson))
+      //   setPersons(persons.concat(newPerson))
+      axios.post('http://localhost:3001/persons', newPerson).then((response) => {
+        console.log(response)
+      })
+
       setNewName('')
       setNewNumber('')
     }
