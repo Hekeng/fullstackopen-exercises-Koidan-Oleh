2f9fa05e31230d13c581742dab5e9e4a9eba8901
Oleh Koidan <o.koidan@nextcontrol.de>
2026-06-19 15:51:56 +0200

    part2_2.18_save

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,254 @@
+import { useEffect, useState } from 'react'
+import axiosServices from './services/axiosServices'
+import Notification from './components/Notification'
+
+const List = ({ countries }) => {
+  if (countries.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+    // result = ['Too many matches, specify another filter']
+  } else if (countries.length <= 10 && countries.length >= 1) {
+    return filtretList.map((item) => <div key={item.name.common}>{item.name.common}</div>)
+  }
+  //   else if (filtretList.length === 1) {
+  //     result = filtretList[0]
+  //   } else {
+  //     result = ['No matches found, please try again']
+  //   }
+
+  //   return (
+  //     <>
+  //       {countries.map((item) => {
+  //         return <div> {item}</div>
+  //       })}
+  //     </>
+  //   )
+}
+
+const APP = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState('')
+  const [displayedCountries, setDisplayedCountries] = useState('')
+
+  useEffect(() => {
+    axiosServices.getAll().then((response) => {
+      //   console.log('response :', response)
+      setCountries(response)
+    })
+    // console.log('Countries downloadet')
+  }, [])
+
+  const handleSetSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const serchCountry = () => {
+    //countries[1].name.common
+
+    const filtretList = countries.filter((item) => item.name.common.toLowerCase().includes(search.toLowerCase()))
+
+    console.log('filtretList : ', filtretList)
+
+    // setDisplayedCountries(filtretList)
+    // let result
+
+    // if (filtretList.length > 10) {
+    //   result = ['Too many matches, specify another filter']
+    // } else if (filtretList.length <= 10 && filtretList.length >= 1) {
+    //   result = filtretList.map((item) => item.name.common)
+    // } else if (filtretList.length === 1) {
+    //   result = filtretList[0]
+    // } else {
+    //   result = ['No matches found, please try again']
+    // }
+
+    // console.log('result : ', result)
+
+    return filtretList
+    // setDisplayedCountries(displayedList)
+  }
+
+  // const perem = serchCountry()
+
+  return (
+    <>
+      find countries <input value={search} onChange={handleSetSearch} />
+      <List countries={serchCountry()} />
+      <Notification />
+    </>
+  )
+}
+
+export default APP
+
+// import { useEffect, useState } from 'react'
+// import axiosServices from './services/axiosServices'
+// import Notification from './components/Notification'
+
+// const Filter = ({ newSearch, handleSetSearch }) => {
+//   return (
+//     <div>
+//       filter shown with <input value={newSearch} onChange={handleSetSearch} />
+//     </div>
+//   )
+// }
+
+// const PersonForm = ({ handleSetNewName, newName, handleNameChange, newNumber, handleTelChange }) => {
+//   return (
+//     <form onSubmit={handleSetNewName}>
+//       <div>
+//         name: <input value={newName} onChange={handleNameChange} />
+//         <div>
+//           number: <input value={newNumber} onChange={handleTelChange} />
+//         </div>
+//       </div>
+//       <div>
+//         <button type="submit">add</button>
+//       </div>
+//     </form>
+//   )
+// }
+
+// const Persons = ({ personsToShow, dellPerson }) => {
+//   return (
+//     <>
+//       {personsToShow.map((person) => (
+//         <div key={person.id}>
+//           <span>
+//             {person.name} {person.number}
+//           </span>
+//           <button onClick={() => dellPerson(person)}>delete</button>
+//         </div>
+//       ))}
+//     </>
+//   )
+// }
+
+// const App = () => {
+//   const [persons, setPersons] = useState([])
+//   const [newName, setNewName] = useState('')
+//   const [newNumber, setNewNumber] = useState('')
+//   const [newSearch, setSearch] = useState('')
+
+//   const [message, setMessage] = useState({ message: null, messageType: null })
+
+//   useEffect(() => {
+//     axiosServices.getAll().then((response) => {
+//       setPersons(response)
+//     })
+//   }, [])
+
+//   const handleNameChange = (event) => {
+//     setNewName(event.target.value)
+//   }
+
+//   const handleTelChange = (event) => {
+//     setNewNumber(event.target.value)
+//   }
+
+//   const handleSetSearch = (event) => {
+//     setSearch(event.target.value)
+//   }
+
+//   const handleSetNewName = (event) => {
+//     event.preventDefault()
+
+//     const newPerson = {
+//       name: newName,
+//       number: newNumber,
+//     }
+
+//     const personId = checkRepeatedName(newPerson.name)
+
+//     if (personId) {
+//       if (window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
+//         axiosServices
+//           .update(personId, newPerson)
+//           .then((response) => {
+//             let updatedPersons = persons.filter((item) => item.id != personId)
+//             updatedPersons = updatedPersons.concat(response)
+//             setPersons(updatedPersons)
+//             showMessage(`${newPerson.name} was updated!`, 'success')
+//           })
+//           .catch((error) => {
+//             showMessage(`Information about ${newPerson.name} has already been removed from the server.`, 'error')
+//           })
+//         setNewName('')
+//         setNewNumber('')
+//       } else {
+//         return
+//       }
+//     } else {
+//       axiosServices.create(newPerson).then((response) => {
+//         const newPersons = persons.concat(response)
+//         setPersons(newPersons)
+//         showMessage(`${newPerson.name} was created!`, 'success')
+//       })
+
+//       setNewName('')
+//       setNewNumber('')
+//     }
+//   }
+
+//   const checkRepeatedName = (personName) => {
+//     let elementId
+//     persons.forEach((element) => {
+//       if (element.name === personName) {
+//         elementId = element.id
+//       }
+//     })
+
+//     return elementId
+//   }
+
+//   const personsToShow = newSearch
+//     ? persons.filter((item) => item.name.toLowerCase().includes(newSearch.toLowerCase()))
+//     : persons
+
+//   const dellPerson = (person) => {
+//     if (window.confirm('Do you really want to delete this person?')) {
+//       const newPersons = persons.filter((item) => item.id != person.id)
+//       axiosServices
+//         .remove(person.id)
+//         .then(() => {
+//           setPersons(newPersons)
+//           showMessage(`Person ${person.name} successfully deleted!`, 'success')
+//         })
+//         .catch((error) => {
+//           showMessage(`Information about ${person.name} has already been removed from the server.`, 'error')
+//           setPersons(newPersons)
+//         })
+//     } else {
+//       showMessage(`Person ${person.name} deleting cancelled`, 'success')
+//     }
+//   }
+
+//   const showMessage = (mess, type) => {
+//     setMessage({ message: mess, messageType: type })
+
+//     setTimeout(() => {
+//       setMessage({ message: null, messageType: null })
+//     }, 5000)
+//   }
+
+//   return (
+//     <div>
+//       <h2>Phonebook</h2>
+//       <Notification message={message.message} messageType={message.messageType} />
+//       <Filter newSearch={newSearch} handleSetSearch={handleSetSearch} />
+
+//       <h3>Add a new</h3>
+//       <PersonForm
+//         handleSetNewName={handleSetNewName}
+//         newName={newName}
+//         handleNameChange={handleNameChange}
+//         newNumber={newNumber}
+//         handleTelChange={handleTelChange}
+//       />
+
+//       <h3>Numbers</h3>
+//       <Persons personsToShow={personsToShow} dellPerson={dellPerson} />
+//     </div>
+//   )
+// }
+
+// export default App
